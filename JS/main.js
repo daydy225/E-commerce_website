2f4e65e95aee85a3af8a7651d78b1ca716c05ca3
daydy225 <dabelydadi@gmail.com>
2021-10-10 13:06:23 +0000

    Ecommerce website
    Creation sticky effect and custom scrollbar

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,36 @@
 const navBouton = document.querySelector('.header1_btn');
 
-
-
 navBouton.addEventListener('click', () => {
   document.body.classList.toggle('nav-open')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+const navSticky = document.querySelector('.header2 .nav')
+
+const sticky = navSticky.offsetTop
+
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navSticky.classList.add("sticky")
+    } else {
+    navSticky.classList.remove("sticky")
+    }
+}
+
+window.onload = () => {
+  if (window.pageYOffset >= sticky) {
+    navSticky.classList.add("sticky")
+    } else {
+    navSticky.classList.remove("sticky")
+    }
+}
